840c1b4507facfce74f36da64cdc4e093bd3b325
Steve Purcell <steve@sanityinc.com>
2019-06-22 09:26:14 +1200

    Apply hindent on save

---
+++ .dir-locals.el
@@ -0,0 +1,10 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((haskell-mode
+  (mode . hindent)
+  (hindent-reformat-buffer-on-save . t)))
+
+
+
+
